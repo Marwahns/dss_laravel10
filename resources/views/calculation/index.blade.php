41fa3b8943fa1d8c416a069158dad692a9737664
Marwah Nur Shafira <marwahnurshafira1705@gmail.com>
2023-07-09 13:09:37 +0700

    Calculating the utility measure (S) and (R) of each alternative

--- resources/views/calculation/index.blade.php
+++ resources/views/calculation/index.blade.php
@@ -567,6 +567,134 @@
                         </div>
                     </div>
                 </div>
+
+                {{-- Langkah Keenam: Menghitung utility measure (S) dan (R) dari setiap alternatif --}}
+                <div
+                    class="relative flex flex-col min-w-0 mb-6 break-words bg-white border-0 border-transparent border-solid shadow-soft-xl rounded-2xl bg-clip-border">
+                    <div class="p-6 pb-0 mb-0 border-b-0 border-b-solid rounded-t-2xl border-b-transparent">
+                        <div class="flex flex-wrap -mx-3 mb-4">
+                            <div class="flex items-center flex-none max-w-full px-3">
+                                <h6 class="mb-0" id="SubTitle"><b>Langkah keenam:</b> Menghitung utility measure (S)
+                                    dan (R) dari setiap alternatif
+                                </h6>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="flex-auto px-0 pt-0 pb-2">
+                        <div class="p-0 overflow-x-auto">
+
+                            @php
+                                $weights = [];
+                                
+                                // Retrieve the weights from the criteria table
+                                foreach ($criterias as $criteria) {
+                                    $weights[] = $criteria->weight;
+                                }
+                                
+                                $utilityMeasuresS = [];
+                                $utilityMeasuresR = [];
+                                
+                                // Iterate over each row of the matrix
+                                foreach ($matrixNormalized as $row) {
+                                    $S = 0;
+                                
+                                    // Sum the values multiplied by the weight in the row
+                                    foreach ($row as $index => $value) {
+                                        $S += $value * $weights[$index];
+                                    }
+                                
+                                    $utilityMeasuresS[] = $S;
+                                
+                                    $maxValue = max($row); // Get the maximum value in the row
+                                
+                                    // Find the index of the maximum value in the row
+                                    $index = array_search($maxValue, $row);
+                                
+                                    // Retrieve the weight for the corresponding criterion
+                                    $weight = $weights[$index];
+                                
+                                    // Calculate the utility measure (R) by multiplying the maximum value with the weight
+                                    $R = $maxValue * $weight;
+                                
+                                    $utilityMeasuresR[] = $R;
+                                }
+                            @endphp
+
+                            <table class="items-center w-full mb-0 align-top border-gray-200 text-slate-500">
+                                <thead class="align-bottom">
+                                    <tr>
+                                        <!-- Nomor -->
+                                        <th
+                                            class="px-6 py-3 font-bold text-center uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
+                                            #
+                                        </th>
+
+                                        <!-- Alternative -->
+                                        <th
+                                            class="px-6 py-3 font-bold text-left uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
+                                            Alternative</th>
+
+                                        <th
+                                            class="px-6 py-3 font-bold text-left uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
+                                            S</th>
+
+                                        <th
+                                            class="px-6 py-3 font-bold text-left uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
+                                            R</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @php
+                                        $i = 1;
+                                    @endphp
+                                    {{-- Iterasi alternatif --}}
+                                    @forelse ($getAlternative as $index => $alternative)
+                                        <tr>
+                                            <!-- Nomor -->
+                                            <td
+                                                class="px-6 py-3 font-bold text-center align-middle bg-transparent border-b tracking-none whitespace-nowrap text-slate-400">
+                                                <span
+                                                    class="text-xs font-semibold leading-tight text-slate-400">{{ $i++ }}</span>
+                                            </td>
+
+                                            <!-- Alternative -->
+                                            <td
+                                                class="p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
+                                                <div class="flex px-2 py-1">
+                                                    <div class="flex flex-col justify-center">
+                                                        <p class="mb-0 text-xs leading-tight text-slate-400">
+                                                            {{ $alternative['nama_alternative'] }}
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </td>
+
+                                            <td
+                                                class="px-6 py-3 font-bold lign-middle bg-transparent border-b tracking-none whitespace-nowrap text-slate-400">
+                                                <span
+                                                    class="text-xs font-semibold leading-tight text-slate-400">{{ number_format($utilityMeasuresS[$index], 2) }}</span>
+                                            </td>
+
+                                            <td
+                                                class="px-6 py-3 font-bold lign-middle bg-transparent border-b tracking-none whitespace-nowrap text-slate-400">
+                                                <span
+                                                    class="text-xs font-semibold leading-tight text-slate-400">{{ number_format($utilityMeasuresR[$index], 2) }}</span>
+                                            </td>
+                                        </tr>
+                                    @empty
+                                        <div class="alert alert-danger flex justify-center">
+                                            Data Post belum Tersedia.
+                                        </div>
+                                    @endforelse
+
+                                </tbody>
+                            </table>
+                            {{ $samples->links('pagination::tailwind') }}
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
 
