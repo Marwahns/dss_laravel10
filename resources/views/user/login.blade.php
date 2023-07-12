3ee33003fa12e35ef68ea47e1d6c62005c3ceae8
Michael Natanael <mnatanael87@gmail.com>
2023-07-12 09:54:50 +0700

    Remove comments in login.blade

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -30,7 +30,7 @@
         <!-- Navbar -->
         <nav class="absolute top-0 left-0 right-0 z-30 flex flex-wrap items-center px-4 py-2 mx-6 my-4 shadow-soft-2xl rounded-blur bg-white/80 backdrop-blur-2xl backdrop-saturate-200 lg:flex-nowrap lg:justify-start">
           <div class="flex items-center justify-between w-full p-0 pl-6 mx-auto flex-wrap-inherit">
-            <a class="py-2.375 text-sm mr-4 ml-4 whitespace-nowrap font-bold text-slate-700 lg:ml-0" href="../pages/dashboard.html">
+            <a class="py-2.375 text-sm mr-4 ml-4 whitespace-nowrap font-bold text-slate-700 lg:ml-0" href="{{ route('login.index') }}">
               <img src="./assets/img/logo-ct.png" class="inline h-full max-w-full transition-all duration-200 ease-nav-brand max-h-8" alt="main_logo" />
               <span class="ml-1 font-semibold transition-all duration-200 ease-nav-brand">Scholarship Recommendation</span>
             </a>
@@ -43,40 +43,7 @@
             </button>
             <div navbar-menu class="items-center flex-grow overflow-hidden transition-all duration-500 ease-soft lg-max:max-h-0 basis-full lg:flex lg:basis-auto">
               <ul class="flex flex-col pl-0 mx-auto mb-0 list-none lg:flex-row xl:ml-auto">
-                <!-- <li>
-                  <a class="flex items-center px-4 py-2 mr-2 font-normal transition-all lg-max:opacity-0 duration-250 ease-soft-in-out text-sm text-slate-700 lg:px-2" aria-current="page" href="../pages/dashboard.html">
-                    <i class="mr-1 fa fa-chart-pie opacity-60"></i>
-                    Dashboard
-                  </a>
-                </li>
-                <li>
-                  <a class="block px-4 py-2 mr-2 font-normal transition-all lg-max:opacity-0 duration-250 ease-soft-in-out text-sm text-slate-700 lg:px-2" href="../pages/profile.html">
-                    <i class="mr-1 fa fa-user opacity-60"></i>
-                    Profile
-                  </a>
-                </li>
-                <li>
-                  <a class="block px-4 py-2 mr-2 font-normal transition-all lg-max:opacity-0 duration-250 ease-soft-in-out text-sm text-slate-700 lg:px-2" href="../pages/sign-up.html">
-                    <i class="mr-1 fas fa-user-circle opacity-60"></i>
-                    Sign Up
-                  </a>
-                </li>
-                <li>
-                  <a class="block px-4 py-2 mr-2 font-normal transition-all lg-max:opacity-0 duration-250 ease-soft-in-out text-sm text-slate-700 lg:px-2" href="../pages/sign-in.html">
-                    <i class="mr-1 fas fa-key opacity-60"></i>
-                    Sign In
-                  </a>
-                </li> -->
               </ul>
-              <!-- online builder btn  -->
-              <!-- <li class="flex items-center">
-                  <a
-                    class="leading-pro ease-soft-in text-fuchsia-500 border-fuchsia-500 text-xs tracking-tight-soft bg-150 bg-x-25 rounded-3.5xl hover:border-fuchsia-500 hover:scale-102 hover:text-fuchsia-500 active:hover:border-fuchsia-500 active:hover:scale-102 active:hover:text-fuchsia-500 active:opacity-85 active:shadow-soft-xs active:bg-fuchsia-500 active:border-fuchsia-500 mr-2 mb-0 inline-block cursor-pointer border border-solid bg-transparent py-2 px-8 text-center align-middle font-bold uppercase shadow-none transition-all hover:bg-transparent hover:opacity-75 hover:shadow-none active:scale-100 active:text-white active:hover:bg-transparent active:hover:opacity-75 active:hover:shadow-none"
-                    target="_blank"
-                    href="https://www.creative-tim.com/builder/soft-ui?ref=navbar-dashboard&amp;_ga=2.76518741.1192788655.1647724933-1242940210.1644448053"
-                    >Online Builder</a
-                  >
-                </li> -->
               <ul class="hidden pl-0 mb-0 list-none lg:block lg:flex-row">
                 <li>
                   <a href="https://michael2002porto.github.io/" target="_blank" class="leading-pro hover:scale-102 hover:shadow-soft-xs active:opacity-85 ease-soft-in text-xs tracking-tight-soft shadow-soft-md bg-150 bg-x-25 bg-gradient-to-tl from-gray-900 to-slate-800 rounded-3.5xl mb-0 mr-1 inline-block cursor-pointer border-0 bg-transparent px-8 py-2 text-center align-middle font-bold uppercase text-white transition-all">Contact Us</a>
@@ -121,7 +88,7 @@
                 <div class="p-6 px-1 pt-0 text-center bg-transparent border-t-0 border-t-solid rounded-b-2xl lg:px-2">
                   <p class="mx-auto mb-6 leading-normal text-sm">
                     Don't have an account?
-                    <a href="../pages/sign-up.html" class="relative z-10 font-semibold text-transparent bg-gradient-to-tl from-blue-600 to-cyan-400 bg-clip-text">Sign up</a>
+                    <a href="https://michael2002porto.github.io/" class="relative z-10 font-semibold text-transparent bg-gradient-to-tl from-blue-600 to-cyan-400 bg-clip-text">Contact admin</a>
                   </p>
                 </div>
               </div>
@@ -139,14 +106,6 @@
   <footer class="py-12">
     <div class="container">
       <div class="flex flex-wrap -mx-3">
-        <!-- <div class="flex-shrink-0 w-full max-w-full mx-auto mb-6 text-center lg:flex-0 lg:w-8/12">
-          <a href="javascript:;" target="_blank" class="mb-2 mr-4 text-slate-400 sm:mb-0 xl:mr-12"> Company </a>
-          <a href="javascript:;" target="_blank" class="mb-2 mr-4 text-slate-400 sm:mb-0 xl:mr-12"> About Us </a>
-          <a href="javascript:;" target="_blank" class="mb-2 mr-4 text-slate-400 sm:mb-0 xl:mr-12"> Team </a>
-          <a href="javascript:;" target="_blank" class="mb-2 mr-4 text-slate-400 sm:mb-0 xl:mr-12"> Products </a>
-          <a href="javascript:;" target="_blank" class="mb-2 mr-4 text-slate-400 sm:mb-0 xl:mr-12"> Blog </a>
-          <a href="javascript:;" target="_blank" class="mb-2 mr-4 text-slate-400 sm:mb-0 xl:mr-12"> Pricing </a>
-        </div> -->
         <div class="flex-shrink-0 w-full max-w-full mx-auto mt-2 mb-6 text-center lg:flex-0 lg:w-8/12">
           <a href="javascript:;" target="_blank" class="mr-6 text-slate-400">
             <span class="text-lg fab fa-dribbble"></span>
